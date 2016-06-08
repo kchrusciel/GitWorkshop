ef041e229e25f8ac8e2005c397b7309de194bbe7
kchrusciel <krzysztof.chrusciel@outlook.com>
2016-06-08 11:20:34 +0200

    Add conflict implementation

---
+++ src/main/java/conflict/Conflict.java
@@ -0,0 +1,17 @@
+package conflict;
+
+/**
+ * Created by krzysztof.chrusciel on 2016-06-08.
+ */
+public class Conflict {
+
+    private String name;
+
+    public Conflict(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
